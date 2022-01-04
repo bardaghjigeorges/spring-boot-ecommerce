e385c89006a83c6d20ba4c950e91259d6721481a
bardaghjigeorges <georges.bardaghji@etu.univ-amu.fr>
2022-01-04 15:05:15 +0100

    Initial commit

---
+++ src/main/java/com/luv2code/springbootecommerce/dao/ProductCategoryRepository.java
@@ -0,0 +1,14 @@
+package com.luv2code.springbootecommerce.dao;
+
+import com.luv2code.springbootecommerce.entity.ProductCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.mapping.CollectionResourceMapping;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@RepositoryRestResource(collectionResourceRel = "productCategory", path ="product-category" )
+@CrossOrigin("http://localhost:4200")
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
+
+}
